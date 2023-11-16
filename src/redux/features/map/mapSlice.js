b26c8a22f5e8c90203599ecfd7e76ff8632a4730
Franco Insaurralde <francoenzo1999@hotmail.com>
2023-11-16 17:50:35 -0300

    preuba franci

--- src/redux/features/map/mapSlice.js
+++ src/redux/features/map/mapSlice.js
@@ -3,7 +3,10 @@ En Redux Toolkit, el término "slice" se refiere a una porción de la tienda glo
 
 Por ejemplo, si tu aplicación tiene una función de autenticación, podrías crear un slice para manejar el estado de la autenticación. Este slice contendría los reducers y actions necesarios para manejar el inicio de sesión, el cierre de sesión, el registro de usuarios, etc.
 
+
 El uso de slices en Redux Toolkit es una forma de organizar el código de tu aplicación de manera más modular y fácil de mantener. Cada slice se puede escribir y probar de manera independiente, lo que permite un desarrollo más rápido y un código más limpio. Además, Redux Toolkit proporciona herramientas para generar automáticamente los reducers y actions básicos para cada slice, lo que reduce la cantidad de código que debes escribir manualmente.
+
+sdsojdfdifjpreuba
 */
 
 
